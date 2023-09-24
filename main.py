9d53d886c60109790fef56ba931b461fa57a83d3
FlaviaLopes <flavithalopes@hotmail.com>
2023-09-24 15:37:03 -0300

    Leitura de todos os arquivos de previsão altera main; lista todos os arquivos em forecast_files; extrai a data do dia previsto do nome do arquivo; itera, lê e concatena todos os arquivos adicionando a data do dia previsto.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import re
 import time
 from functools import wraps
+import os
 
 
 def read_data_file(file_path: str) -> pd.DataFrame:
@@ -32,9 +33,21 @@ def apply_contour(contour_df: pd.DataFrame, data_df: pd.DataFrame) -> pd.DataFra
 
 
 def main() -> None:
-    contour_df: pd.DataFrame = read_contour_file('PSATCMG_CAMARGOS.bln')
-    data_df: pd.DataFrame = read_data_file('forecast_files/ETA40_p011221a021221.dat')
-    contour_df: pd.DataFrame = apply_contour(contour_df=contour_df, data_df=data_df)
+    forecast_files_dir = './forecast_files'
+    forecast_files = os.listdir(forecast_files_dir)
+    forecast_files = [os.path.join(forecast_files_dir, it) for it in forecast_files]
+    forecasted_date_pattern = re.compile(r'a(\d{6})\.dat')
+    forecasted_dates = [forecasted_date_pattern.split(it)[1] for it in forecast_files]
+    forecasted_dates = [pd.to_datetime(it, format='%d%m%y').strftime("%d/%m/%y") for it in forecasted_dates]
+
+    data_df = pd.DataFrame()
+    for filename, forecasted_date in zip(forecast_files, forecasted_dates):
+        temp = read_data_file(filename)
+        temp['forecasted_date'] = forecasted_date
+        data_df = pd.concat([data_df, temp], axis=0)
+    print(data_df.shape, data_df.head())
+    # contour_df: pd.DataFrame = read_contour_file('PSATCMG_CAMARGOS.bln')
+    # contour_df: pd.DataFrame = apply_contour(contour_df=contour_df, data_df=data_df)
 
 
 if __name__ == '__main__':
